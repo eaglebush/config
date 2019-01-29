0770779f9239dfebec920e006de33802cfe517c8
Zaldy Baguinon (@IT) <zaldy.baguinon@vdimdci.com.ph>
2019-01-29 11:06:22 +0800

    Made connection information into an array. Added notifications. Made function to retrieve an item in the arrays.

--- configuration.go
+++ configuration.go
@@ -33,16 +33,33 @@ type MailServer struct {
 	SenderName string
 }
 
+//DatabaseInfo - database configuration setting
+type DatabaseInfo struct {
+	ID                string //A unique ID that will identify the connection to a database
+	ConnectionString  string //ConnectionString specific to the database
+	DriverName        string //DriverName needs to be specified depending on the driver id used by the Go database driver
+	StorageType       string //StorageType: FILE for filebased database such as Access, SQlite or LocalDB. SERVER for SQL Server, MySQL etc
+	GroupID           string `json:"GroupID,omitempty"` //GroupID allows us to get groups of connection
+	SequenceGenerator SequenceGeneratorInfo
+}
+
+//NotificationInfo - notification configuration setting
+type NotificationInfo struct {
+	ID            string
+	FullName      string
+	EmailAddress  string
+	MessengerName string
+}
+
 //Configuration - for various configuration settings. This struct can be modified depending on the requirement.
 type Configuration struct {
-	HostPort         int
-	ConnectionString string
-	ConvergenceHost  string
-	DriverName       string
-	HMAC             string
-	MailServer       MailServer
-	SequenceInfo     SequenceGeneratorInfo
-	Domains          []DomainInfo
+	HostPort          int
+	HMAC              string
+	MailServer        MailServer         `json:"MailServer,omitempty"`
+	DefaultDatabaseID string             `json:"DefaultDatabaseID,omitempty"`
+	Databases         []DatabaseInfo     `json:"Databases,omitempty"`
+	Domains           []DomainInfo       `json:"Domains,omitempty"`
+	NotifyRecipients  []NotificationInfo `json:"NotifyRecipients,omitempty"`
 }
 
 // LoadConfig - load configuration file and return a configuration
@@ -67,4 +84,49 @@ func LoadConfig(fileName string) (*Configuration, error) {
 	}
 
 	return config, nil
+}
+
+//GetDatabaseInfo - get a database info by ID
+func (c *Configuration) GetDatabaseInfo(ConnectionID string) *DatabaseInfo {
+	for _, v := range c.Databases {
+		if v.ID == ConnectionID {
+			return &v
+		}
+	}
+
+	return nil
+}
+
+//GetDatabaseInfoGroup - get database infos based on the group id
+func (c *Configuration) GetDatabaseInfoGroup(GroupID string) *[]DatabaseInfo {
+	dbgi := make([]DatabaseInfo, 0)
+	for _, v := range c.Databases {
+		if v.GroupID == GroupID {
+			dbgi = append(dbgi, v)
+		}
+	}
+
+	return &dbgi
+}
+
+//GetDomainInfo - get a domain info by name
+func (c *Configuration) GetDomainInfo(DomainName string) *DomainInfo {
+	for _, v := range c.Domains {
+		if v.Name == DomainName {
+			return &v
+		}
+	}
+
+	return nil
+}
+
+//GetNotifyRecipient - get a notification recipient info by name
+func (c *Configuration) GetNotifyRecipient(RecipientID string) *NotificationInfo {
+	for _, v := range c.NotifyRecipients {
+		if v.ID == RecipientID {
+			return &v
+		}
+	}
+
+	return nil
 }
